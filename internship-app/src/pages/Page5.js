adb45d433140026477107190bc540a52c99ae819
steven <steven77thomas.2003@gmail.com>
2024-06-11 20:24:57 +0530

    weather app added and some bugs fixes and style changes

--- internship-app/src/pages/Page5.js
+++ internship-app/src/pages/Page5.js
@@ -1,18 +1,23 @@
 import React from 'react';
 
 // import Header from '../components/Header'
-import Title from '../components/Title';
+//import Title from '../components/Title';
 // import Main from '../components/Main'
 import Footer from '../components/Footer';
+import Weather from '../components/Weather';
+import { WiDayCloudy } from 'react-icons/wi';
 
 
 
 function Page5() {
   return (
-    <div className="App">
+    <div className="page5">
      {/* <Header/> */}
-     <Title title="Weather App"/>
+     {/* <Title title="Weather App"/> */}
      {/* <Main /> */}
+     <h1>
+        <div className='icons'><WiDayCloudy size={50}  /></div>Weather App</h1>
+      <Weather />
      <Footer/>
     </div>
   );
